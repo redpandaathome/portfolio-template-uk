f5c7bd9568a499c76a29bf4246179358aad0c332
redpandaathome <leeyumi415@gmail.com>
2022-01-21 16:36:07 +0900

    +font weight, contact buttons functionality

--- main2.js
+++ main2.js
@@ -1,6 +1,27 @@
 const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const menu = document.querySelector('.navbar__menu');
 
+const linkedInBtn = document.querySelector('.linked-in');
+const emailBtn = document.querySelector('.email');
+
+// NavBar - toggle hamburger bar
 toggleBtn.addEventListener('click', ()=>{
    menu.classList.toggle('active');
+})
+
+// Section - Contact, linked-in not yet message
+linkedInBtn.addEventListener('click', ()=>{
+   alert("To be prepared soon..!")
+})
+
+// Section - Contact, email copy button
+emailBtn.addEventListener('click', ()=> {
+   const targetEmail = 'leeyumi415@gmail.com'
+   navigator.clipboard.writeText(targetEmail)
+   .then(()=>{
+      alert("Successfuly copied to clipboard!")
+   })
+   .catch(()=>{
+      alert("Copying failed..!")
+   })
 })
